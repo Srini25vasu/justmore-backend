c7a9e79e63bdc744964b814f94d663e48e01001b
Srini25vasu <srini25vasu@gmail.com>
2019-11-16 11:56:36 +0100

    First commit

---
+++ src/main/java/com/justmehr/backend/JustmehrBackendApplication.java
@@ -0,0 +1,13 @@
+package com.justmehr.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustmehrBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustmehrBackendApplication.class, args);
+	}
+
+}
